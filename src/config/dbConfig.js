357db20f4b644d48512bc7eabeaa6c8d9338381b
9ruem2 <9ruem2@naver.com>
2024-03-11 09:24:35 +0900

    문제 되돌림

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,41 +1,36 @@
-const AWS = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const mysql = require('mysql');
+const util = require('util'); 
 const configs = require('./configs');
-const logger = require('./loggerConfig')(module); 
+const logger = require('./loggerConfig')(module);
+const dbInfo = { 
+  host: configs.database.host,
+  user: configs.database.user,
+  password: configs.database.password,
+  database: configs.database.name
+};
 
-// AWS S3 설정 초기화
-AWS.config.update({
-    accessKeyId: configs.aws.AWS_ACCESS_KEY, 
-    secretAccessKey: configs.aws.AWS_SECRET_ACCESS_KEY, 
-    region: configs.aws.AWS_REGION
-});
+const dbConfig = {
+  initializeDatabase: async () => {
+    try {
+      const connection = mysql.createConnection(dbInfo); 
 
-const s3 = new AWS.S3(); 
+      const connectAsync = util.promisify(connection.connect).bind(connection);
+      const queryAsync = util.promisify(connection.query).bind(connection);
 
-// multerS3 스토리지 엔진을 사용한 multer 설정
-const multerS3Storage = multerS3({
-    s3: s3, 
-    bucket: configs.aws.S3_BUCKET_LOCATION, 
-    contentType: multerS3.AUTO_CONTENT_TYPE, 
-    acl: 'public-read', 
+      await connectAsync();
+      logger.info('MySQL is connected');
 
-    // 업로드된 파일의 메타데이터 설정 함수
-    metadata: (req, file, cb) => {
-        cb(null, { fieldName: file.fieldname });
-    },
+      const sql = 'UPDATE DEVICES SET Session_exist = REPLACE(Session_exist, "1","0")';
+      await queryAsync(sql);
+      logger.info("DB init success");
 
-    // 파일이 S3에 저장될 때의 파일명과 경로 설정 함수
-    key: (req, file, cb) => {
-        let prefix = 'Smartfarm'; // 파일 저장 경로의 접두어
-        let deviceOwner = req.headers.device_owner; // 디바이스 소유자 정보
-        let deviceName = req.headers.device_name; // 디바이스 이름
-        let fileType = ''; // 파일 타입
-        let filePath = ''; // 최종 파일 경로
+      return connection;
 
-        // 파일 경로와 이름을 결정하는 로직
-        cb(null, filePath);
+    } catch (err) {
+      logger.error('Initialization failed: ' + err.message);
+      throw err;
     }
-});
+  }
+};
 
-module.exports = multer({ storage: multerS3Storage });
+module.exports = dbConfig;
