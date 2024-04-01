8a7bfe140214dac9be77da3de5f5f7fa8a04ae70
9ruem2clear <9ruem2@naver.com>
2024-04-01 13:28:37 +0900

    Redis 적용 전 코드

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -47,3 +47,26 @@ module.exports = function setupUploadMiddleware(dbConnection) {
         }
     };
 };
+
+/*
+Redis 적용 전 코드
+
+// token이 일치하는지 확인하는 로직
+module.exports = function setupUploadMiddleware(dbConnection) {
+    return async (req, res, next) => {
+        try {
+            const result = await deviceRepository.findDeviceNameByOwnerId(req.headers.device_owner, req.headers.device_name, dbConnection);
+            if (result.length > 0 && result[0].http_token == req.headers.token) {
+                logger.info(`Token validation successful for device owner: ${req.headers.device_owner} and device name: ${req.headers.device_name}`);
+                next();
+            } else {
+                logger.error(`Token validation failed for device owner: ${req.headers.device_owner} and device name: ${req.headers.device_name}`);
+                res.status(401).send('HTTP auth (token) error');
+            }
+        } catch (error) {
+            logger.error('Error in token validation middleware:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    };
+};
+*/
